929c805290b8484e941b7e33bdba808eca67499c
BohdanRadchenko <radchenkobs@gmail.com>
2020-04-27 21:21:06 +0300

    added cors origin for Heroku conect to mongo DB

--- app.js
+++ app.js
@@ -5,9 +5,16 @@ const config = require('config')
 const bodyParser = require("body-parser")
 
 const PORT = process.env.PORT || config.get("port") || 5000
-const MONGO_DB = process.env.MONGODB_URL || config.get('mongoURL')
+const MONGODB_URL = process.env.MONGODB_URL || config.get('mongoURL')
+
+corsOptions = {
+    origin: "https://ptm-book.herokuapp.com/",
+    optionsSuccessStatus: 200
+};
 
 const app = express()
+
+app.use(cors(corsOptions));
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.json({extended: true}));
@@ -28,12 +35,13 @@ app.get("*", (req, res) => {
 
 const start = async () => {
     try {
-        await mongoose.connect( MONGO_DB, {
+        await mongoose.connect(MONGODB_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
         })
-        app.listen(PORT, () => console.log(`app hes been started on port ${PORT} ...`))
+        console.log(`MongoDB is Connected... ${MONGODB_URL}`);
+        app.listen(PORT, () => console.log(`App hes been started on port ${PORT} ...`))
     } catch (e) {
         console.log('Server Error', e.message)
         process.exit(1)
